8874ef523f0c897b0554e98a495a442af800f87d
Saurabh Singh <saurabhsingh@Saurabhs-MacBook-Air.local>
2024-11-01 17:34:03 +0530

    header btn functional changes

--- src/Components/HeaderButtons.tsx
+++ src/Components/HeaderButtons.tsx
@@ -2,13 +2,13 @@ import React, { useContext } from 'react';
 import { TaskContext } from './utils/TaskContext';
 
 const FilterButtons = () => {
-  const { setFilter } = useContext(TaskContext);
+  const { setFilter, filter} = useContext(TaskContext);
 
   return (
     <div className='filter-btns'>
-      <button onClick={() => setFilter('all')}>All</button>
-      <button onClick={() => setFilter('completed')}>Completed</button>
-      <button onClick={() => setFilter('incomplete')}>Incomplete</button>
+      <button style={{backgroundColor: filter === 'all' ? '#4caf50' : ''}} onClick={() => setFilter('all')}>All</button>
+      <button style={{backgroundColor: filter === 'completed' ? '#4caf50' : ''}} onClick={() => setFilter('completed')}>Completed</button>
+      <button style={{backgroundColor: filter === 'incomplete' ? '#4caf50' : ''}} onClick={() => setFilter('incomplete')}>Incomplete</button>
     </div>
   );
 };
